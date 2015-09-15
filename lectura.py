f3ab36c11146cb7de4210c0b926b5c576f8dab84
Benguerrero1 <benjaminguerrero1@gmail.com>
2015-09-14 23:23:49 -0300

    Datos extraidos y grafico hecho

---
+++ lectura.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mp
+
+x,y = np.loadtxt('sun_AM0.dat', unpack=True)
+print (x)
+print (y)
+#x= longitud de onda en nm
+#y= espectro del Sol en Watts/(m^2*nm)
+print type(x) #comprueba que es numpy.ndarray (analogo para y)
+x=x*10 #convierte nm en agnstrom
+y=y*0.001 #convierte al sistema cgs
+plt.plot(x,y)
+plt.yscale('log')
+plt.xscale('log') 
+plt.ylabel("Espectro de sol ($\\frac{erg}{s*cm^2*cm}$)", fontsize=20)
+plt.xlabel("Longitud de onda (A)",fontsize=20)
